73247a22c15fb0f1e439300fae2a038cf1754d6d
Manavivian <h.vivian92@gmail.com>
2018-10-04 00:24:00 -0700

    Rough sample of layout

--- photo-ai/src/app/app.module.ts
+++ photo-ai/src/app/app.module.ts
@@ -3,20 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MyNavbarComponent } from './my-navbar/my-navbar.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
-import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
-import { MyTableComponent } from './my-table/my-table.component';
 import {RouterModule} from "@angular/router";
 import {AppRoutingModule} from "./app-routing-module";
+import { HomeComponent } from './home/home.component';
+import {FileUploadModule} from 'primeng/fileupload';
+import {ButtonModule} from 'primeng/button';
+import {ToolbarModule} from 'primeng/toolbar';
+import {SplitButtonModule} from 'primeng/splitbutton';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyNavbarComponent,
-    MyDashboardComponent,
-    MyTableComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +33,11 @@ import {AppRoutingModule} from "./app-routing-module";
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FileUploadModule,
+    ButtonModule,
+    ToolbarModule,
+    SplitButtonModule
   ],
   exports: [RouterModule],
   providers: [],
