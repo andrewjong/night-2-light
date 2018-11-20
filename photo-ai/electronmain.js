d25965055e0ed2f58c6e1c79d654928087681f84
jufa2401 <jufa2401@gmail.com>
2018-11-19 19:12:19 -0800

    i cannot find the how the path error occurs, have to look more into it. I tried changing the structure of the application but i seemed to get the same error

--- photo-ai/electronmain.js
+++ photo-ai/electronmain.js
@@ -4,13 +4,14 @@ const path = require('path');
 const url = require('url');
 const notify = require('electron-main-notification');
 
+
 let win;
 function createWindow () {
   const windowOptions = {width: 1400, height: 900, show:false};
   win = new BrowserWindow(windowOptions); // load the dist folder from Angular
 
-// Open the DevTools optionally:
-  win.webContents.openDevTools();
+
+
   win.on('closed', () => { win = null });
 
   win = Splashscreen.initSplashScreen({
@@ -24,17 +25,21 @@ function createWindow () {
       transparent: true,
     },
   });
-  win.loadURL(url.format({ pathname: path.join(__dirname, 'dist/index.html'), protocol: 'file:', slashes: true }));
+  const fileurl = url.format({ pathname: path.join(__dirname, 'dist/photo-ai/index.html'), protocol: 'file:', slashes: true });
+  console.log(fileurl);
+  win.loadURL(fileurl);
   win.setMenu(null);
+// Open the DevTools optionally:
+  win.webContents.openDevTools();
 }
 
 
-app.setAppUserModelId('Night-2-Light');
+app.setAppUserModelId('Photo-Ai');
 app.on('ready', createWindow);
 app.on('window-all-closed', () => { if (process.platform !== 'darwin') { app.quit() } });
 app.on('activate', () => { if (win === null) { createWindow() } });
 app.on('ready', () => {
-  notify('Welcome!', { body: 'This is Night-2-Light in Electron!' }, () => {
+  notify('Welcome!', { body: 'This is Hangman in Electron!' }, () => {
     console.log('The notification got clicked on!')
   })
 });
